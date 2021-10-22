ac2c5b0c5fbc4e9debe37eec378dab45693524a2
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2021-10-22 16:22:08 +0300

    add xrdb package, organize code

--- config.scm
+++ config.scm
@@ -35,143 +35,146 @@ EndSection
 ")
 
 (operating-system
-  (kernel linux)
-  (locale "en_US.utf8")
-  (host-name "mahmooz")
-  (timezone "Asia/Jerusalem")
+ (kernel linux)
+ (locale "en_US.utf8")
+ (host-name "mahmooz")
+ (timezone "Asia/Jerusalem")
 
-  (keyboard-layout (keyboard-layout "us" "altgr-intl"))
+ (keyboard-layout (keyboard-layout "us" "altgr-intl"))
 
-  (kernel-loadable-modules (list rtl8812au-aircrack-ng-linux-module))
+ (kernel-loadable-modules (list rtl8812au-aircrack-ng-linux-module))
 
-  ;; This is needed to create a bootable USB
-  (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                ;;(timeout 1)
-                (targets (list "/boot/efi"))))
+ ;; This is needed to create a bootable USB
+ (bootloader (bootloader-configuration
+              (bootloader grub-efi-bootloader)
+              ;;(timeout 1)
+              (targets (list "/boot/efi"))))
 
-  (firmware (append (list iwlwifi-firmware)
-                    %base-firmware))
+ (firmware (append (list iwlwifi-firmware)
+                   %base-firmware))
 
-  (users (cons* (user-account
-                  (name "mahmooz")
-                  (group "users")
-                  (supplementary-groups '("wheel" "audio" "adbusers"))
-		  (shell (file-append zsh "/bin/zsh"))
-                  (home-directory "/home/mahmooz"))
-                %base-user-accounts))
+ (users (cons* (user-account
+                (name "mahmooz")
+                (group "users")
+                (supplementary-groups '("wheel" "audio" "adbusers"))
+                (shell (file-append zsh "/bin/zsh"))
+                (home-directory "/home/mahmooz"))
+               %base-user-accounts))
 
-  (packages (append (list
-                     nss-certs ;; for https
+ (packages (append (list
+                    nss-certs ;; for https
 
-                     ;; fonts
-                     fontconfig
-                     font-fantasque-sans
-                     font-dejavu
+                    ;; fonts
+                    fontconfig
+                    font-fantasque-sans
+                    font-dejavu
 
-                     ;; media
-                     mpv feh
-                     sxiv
+                    ;; media
+                    mpv feh
+                    sxiv
 
-                     ;; X
-                     libinput xf86-video-fbdev
-                     xf86-video-nouveau xf86-video-ati xf86-video-vesa
-                     sxhkd
-                     awesome
-                     sxhkd setxkbmap
-                     xorg-server
-                     picom
-                     clipit
-                     rofi
-                     xclip
-                     xset
+                    ;; X
+                    libinput xf86-video-fbdev
+                    xf86-video-nouveau xf86-video-ati xf86-video-vesa
+                    sxhkd
+                    awesome
+                    sxhkd
+                    setxkbmap
+                    xorg-server
+                    picom
+                    clipit
+                    rofi
+                    xclip
+                    xset
+                    xrdb
 
-                     ;; text editors
-                     emacs
-                     neovim
+                    ;; text editors
+                    neovim
 
-                     ;; commandline tools
-                     curl
-                     git
-                     zsh
-                     tmux
-                     alacritty
-                     transmission
-                     bat
-                     clyrics
-                     scrot
-                     adb
-                     ranger
-                     vifm
-                     imagemagick
-                     file
-                     ffmpeg
-                     sshfs
+                    ;; commandline tools
+                    curl
+                    git
+                    zsh
+                    tmux
+                    alacritty
+                    transmission
+                    bat
+                    clyrics
+                    scrot
+                    adb
+                    ranger
+                    vifm
+                    imagemagick
+                    file
+                    ffmpeg
+                    youtube-dl
 
-                     ;; other
-                     libnotify
-                     network-manager
-                     nyxt
-                     rsync
-                     pulseaudio pulsemixer
-                     qutebrowser
-                     firefox
-                     openssh
-                     emacs-guix
-                     cmake
-                     gnu-make
-                     dbus
-                     playerctl
-                     hostapd
-                     flatpak
+                    ;; emacs
+                    emacs
+                    emacs-guix
+                    emacs-geiser
 
-                     ;; rust
-                     rust
-                     rust-cargo-0.53
+                    ;; other
+                    libnotify
+                    network-manager
+                    rsync
+                    pulseaudio pulsemixer
+                    firefox
+                    openssh
+                    cmake
+                    gnu-make
+                    dbus
+                    playerctl
+                    hostapd
+                    flatpak
 
-                     ;; python
-                     python
-                     python-pip
-                     )
-                    %base-packages))
+                    ;; rust
+                    rust
+                    rust-cargo-0.53
 
-  (sudoers-file
-   (plain-file
-    "sudoers"
-    "root ALL=(ALL) ALL
+                    ;; python
+                    python
+                    python-pip
+                    )
+                   %base-packages))
+
+ (sudoers-file
+  (plain-file
+   "sudoers"
+   "root ALL=(ALL) ALL
     %wheel ALL=(ALL) NOPASSWD: ALL"))
 
-  (hosts-file
-   (plain-file
-    "hosts"
-    "10.0.0.50 server"))
+ (hosts-file
+  (plain-file
+   "hosts"
+   "10.0.0.50 server"))
 
-  (services (append (list (service network-manager-service-type)
-                          (udev-rules-service 'android android-udev-rules
-                                              #:groups '("adbusers"))
-                          (service wpa-supplicant-service-type)
-                          (service slim-service-type
-                                   (slim-configuration
-                                    (auto-login? #t)
-                                    (default-user "mahmooz")
-                                    (display ":0")
-                                    (vt "vt2")
-                                    (xorg-configuration (xorg-configuration (extra-config (list %xorg-libinput-config))))))
-                          (service xorg-server-service-type)
-                          (service hostapd-service-type
-                                   (hostapd-configuration
-                                     (interface "wlp0s20f3")
-                                     (ssid "devilspawn")))
-                          (service alsa-service-type (alsa-configuration
-                                                       (pulseaudio? #t))))
-                    %base-services))
+ (services (append (list (service network-manager-service-type)
+                         (udev-rules-service 'android android-udev-rules
+                                             #:groups '("adbusers"))
+                         (service wpa-supplicant-service-type)
+                         (service slim-service-type
+                                  (slim-configuration
+                                   (auto-login? #t)
+                                   (default-user "mahmooz")
+                                   (display ":0")
+                                   (vt "vt2")
+                                   (xorg-configuration (xorg-configuration (extra-config (list %xorg-libinput-config))))))
+                         (service xorg-server-service-type)
+                         (service hostapd-service-type
+                                  (hostapd-configuration
+                                   (interface "wlp0s20f3")
+                                   (ssid "devilspawn")))
+                         (service alsa-service-type (alsa-configuration
+                                                     (pulseaudio? #t))))
+                   %base-services))
 
-  (file-systems (cons* (file-system
-                        (device (file-system-label "guix"))
-                        (mount-point "/")
-                        (type "ext4"))
-                       (file-system
-                        (device "/dev/nvme0n1p1")
-                        (type "vfat")
-                        (mount-point "/boot/efi"))
-                       %base-file-systems)))
+ (file-systems (cons* (file-system
+                       (device (file-system-label "guix"))
+                       (mount-point "/")
+                       (type "ext4"))
+                      (file-system
+                       (device (file-system-label "boot"))
+                       (type "vfat")
+                       (mount-point "/boot/efi"))
+                      %base-file-systems)))
