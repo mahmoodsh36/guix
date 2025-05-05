c33bac9a2381d06fa1d8047b4b70da1de62961e2
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-05-05 18:58:38 +0300

    minor changes

--- minimal-os.scm
+++ minimal-os.scm
@@ -11,8 +11,8 @@
                      gtk compton wm freedesktop xorg xdisorg compression video image-viewers code
                      emacs-xyz python-build)
 
-;(use-modules (packages scrcpy))
-(use-modules (packages sxiv))
+;; (use-modules (packages scrcpy))
+;; (use-modules (packages sxiv))
 
 (define %xorg-libinput-config
   "Section \"InputClass\"
@@ -85,11 +85,54 @@ EndSection
     ;;    "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
     ;;    (openpgp-fingerprint
     ;;     "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+    ;; (channel
+    ;;  (name guix-hpc)
+    ;;  (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git"))
+    ;; (channel
+    ;;   (name 'guix-gaming-games)
+    ;;   (url "https://gitlab.com/guix-gaming-channels/games.git")
+    ;;   (branch "master")
+    ;;   (commit
+    ;;     "6d94f531b17fe17957f90ee1ed70a8ebebf57a82")
+    ;;   (introduction
+    ;;     (make-channel-introduction
+    ;;       "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
+    ;;       (openpgp-fingerprint
+    ;;         "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
+    ;; (channel
+    ;;   (name 'guix-past)
+    ;;   ;; (url "https://gitlab.inria.fr/guix-hpc/guix-past")
+    ;;   (url "https://codeberg.org/guix-science/guix-past")
+    ;;   (branch "master")
+    ;;   (commit
+    ;;     "8353f5e12b31a6f477956a10f68d271d805a634f")
+    ;;   (introduction
+    ;;     (make-channel-introduction
+    ;;       "0c119db2ea86a389769f4d2b9c6f5c41c027e336"
+    ;;       (openpgp-fingerprint
+    ;;         "3CE4 6455 8A84 FDC6 9DB4  0CFB 090B 1199 3D9A EBB5"))))
     (channel
      (name 'guix)
      (url "https://codeberg.org/guix/guix-mirror")
-     ;; (branch "master")
-     )
+     (branch "master")
+     (commit
+      "e124661486ec722b5c09a94b416f0104b9dde5a4")
+     (introduction
+      (make-channel-introduction
+       "9edb3f66fd807b096b48283debdcddccfea34bad"
+       (openpgp-fingerprint
+        "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+    (channel
+     (name 'rde)
+     (url "https://git.sr.ht/~abcdw/rde")
+     (branch "master")
+     (commit
+      "eaf0ec48cb06ee5dea1f86a502255e14c2b3973b")
+     (introduction
+      (make-channel-introduction
+       "257cebd587b66e4d865b3537a9a88cccd7107c95"
+       (openpgp-fingerprint
+        "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
     (channel
      (name 'nonguix)
      (url "https://gitlab.com/nonguix/nonguix")
@@ -98,14 +141,14 @@ EndSection
        "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
        (openpgp-fingerprint
         "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-    (channel
-     (name 'guix-science-nonfree)
-     (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
-     (introduction
-      (make-channel-introduction
-       "58661b110325fd5d9b40e6f0177cc486a615817e"
-       (openpgp-fingerprint
-        "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+    ;; (channel
+    ;;  (name 'guix-science-nonfree)
+    ;;  (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
+    ;;  (introduction
+    ;;   (make-channel-introduction
+    ;;    "58661b110325fd5d9b40e6f0177cc486a615817e"
+    ;;    (openpgp-fingerprint
+    ;;     "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
     )
    %default-channels))
 
@@ -144,16 +187,18 @@ EndSection
                  (authorized-keys
                   (cons* %nonguix-key
                          %default-authorized-guix-keys))
+                 ;; https://libreplanet.org/wiki/Group:Guix/Mirrors
                  (substitute-urls
                   '("https://substitutes.nonguix.org"
                     "https://guix.bordeaux.inria.fr/"
                     "https://hydra-guix-129.guix.gnu.org/"
                     "https://bordeaux-singapore-mirror.cbaines.net/"
                     "https://packages.pantherx.org/"
-                    "https://mirror.sjtu.edu.cn/guix/"
-                    "http://ci.guix.trop.in"
-                    "https://guix.tobias.gr/substitutes/"
-                    "http://substitutes.guix.sama.re/"))
+                    ;; "https://mirror.sjtu.edu.cn/guix/"
+                    ;; "http://ci.guix.trop.in"
+                    ;; "https://guix.tobias.gr/substitutes/"
+                    ;; "http://substitutes.guix.sama.re/"
+                    ))
                  (channels %my-channels)
                  ;; (extra-options '("--max-jobs=6"
                  ;;                  "--cores=0"))
